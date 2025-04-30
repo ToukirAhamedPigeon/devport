2853eba57c8fbaa984cb489083f5c09a1a347777
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-04-30 23:33:11 +0600

    Final

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaCode, FaHome, FaUser } from "react-icons/fa";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
-   <main className="relative bg-black-100 text-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+   <main className="relative bg-black-100 text-white flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
     <div className="max-w-7xl w-full">
       <FloatingNav navItems={navItems} />
       <Hero />
@@ -18,6 +18,7 @@ export default function Home() {
       <Clients />
       <Experience />
       <Approach />
+      <Footer />
     </div>
    </main>
   );
